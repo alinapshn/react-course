a5a0d5667e32ca6c1b43a1e6635361bee3eb1bb9
alinapshn <alinapshenichnaya06@gmail.com>
2025-10-29 17:48:50 +0100

    move image assets from public to src/assets/images

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,6 +1,7 @@
 import "./OrdersPage.css";
 import { Header } from "../components/Header";
 import { Link } from "react-router";
+import BuyAgainIcon from '../assets/images/icons/buy-again.png'
 
 export function OrdersPage() {
   return (
@@ -49,7 +50,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -78,7 +79,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -127,7 +128,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
